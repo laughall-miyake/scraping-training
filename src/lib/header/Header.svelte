3997db460690ffe37e822c212b1349ac38a0f09c
laughall-miyake <t-miyake@laughall.co.jp>
2022-08-16 23:15:03 +0900

    change header

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -4,8 +4,8 @@
 <header class="bg-white shadow">
 	<div class="max-w-[1080px] h-[70px] flex items-center m-auto relative ">
 		<a href="/" class="flex flex-col">
-			<span class="text-2xl font-bold">スクレイピング練習サイト（beta-）</span>
-			<span class="text-sm">だれでも使えて制限なし</span>
+			<span class="text-2xl font-bold">スクレイピング練習場（ベータ）</span>
+			<span class="text-sm">だれでも使えて制限なし！</span>
 		</a>
 		<div class="absolute right-0">
 			<a href="https://github.com/laughall-miyake/scraping-training"
